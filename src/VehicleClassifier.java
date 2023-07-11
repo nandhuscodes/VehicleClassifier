c110440eaf5ec14a21d692e8632c61fa6c0b14b6
Nandhini S <nandhini18vgr@gmail.com>
2023-07-11 11:50:07 +0530

    added so-and-so things in my code

--- src/VehicleClassifier.java
+++ src/VehicleClassifier.java
@@ -1,19 +1,31 @@
-class Vehicle{
+abstract class Vehicle{
     String vehicleName;
 
     public Vehicle(String vehicleName) {
         this.vehicleName = vehicleName;
     }
-    public void start(){
+    abstract void start();
+    abstract void stop();
+    /*public void start(){
         System.out.println(vehicleName+" started.");
     }
     public void stop(){
         System.out.println(vehicleName+" stopped.");
-    }
+    }*/
 }
 class Car extends Vehicle{
     public Car(String vehicleName){
         super(vehicleName);
+    }
+
+    @Override
+    void start() {
+        System.out.println("Car started");
+    }
+
+    @Override
+    void stop() {
+        System.out.println("Car stopped");
     }
 }
 class Motorcycle extends Vehicle{
@@ -21,12 +33,22 @@ class Motorcycle extends Vehicle{
         super(vehicleName);
     }
 
+    @Override
+    void start() {
+        System.out.println("Motorcycle started");
+    }
+
+    @Override
+    void stop() {
+        System.out.println("Motorcycle stopped");
+    }
+
 }
 public class VehicleClassifier {
     public static void main(String[] args) {
-        Vehicle vehicle = new Vehicle("Vehicle");
+        /*Vehicle vehicle = new Vehicle("Vehicle");
         vehicle.start();
-        vehicle.stop();
+        vehicle.stop();*/
         Car car = new Car("Car");
         car.start();
         car.stop();
